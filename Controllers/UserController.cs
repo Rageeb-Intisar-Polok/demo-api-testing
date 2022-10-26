bbcd72da367af3b6dd3944cddd59fec556b83dc9
Rageeb Intisar Polok <rageebintisarpolok@gmail.com>
2022-10-26 23:51:25 +0600

    first commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using Choton_1_demo_api.Models;
+using CoreApiResponse;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Choton_1_demo_api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : BaseController
+    {
+        User _user;
+        public UserController()
+        {
+            _user = new User();
+            _user.Name = "Rageeb Intisar";
+            _user.Password = "demo password for testing";
+        }
+
+        [HttpGet]
+        public IActionResult getInfo()
+        {
+            try
+            {
+                return CustomResult("Data loaded successfully. ", _user);
+            }
+            catch(Exception ex)
+            {
+                return CustomResult(ex.Message, System.Net.HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult setName(string proposed_name)
+        {
+            try
+            {
+                _user.Name = proposed_name;
+                return CustomResult("Name is set successfully", _user);
+            }
+            catch(Exception ex)
+            {
+                return CustomResult(ex.Message, System.Net.HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpPost]
+
+        public IActionResult setPassword(string proposed_password)
+        {
+            try
+            {
+                _user.Password = proposed_password;
+                return CustomResult("Password is set successfully", _user);
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, System.Net.HttpStatusCode.BadRequest);
+            }
+        }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Choton_1_demo_api.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Password { get; set; } // here password is not encrypted for testing purposes only. use it as it is.
+    }
+}
